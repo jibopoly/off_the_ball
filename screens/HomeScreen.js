0cc0c228be1234ff77a57375cd49ecb0887fc3db
jibopoly <michaelclose23@googlemail.com>
2019-03-03 17:19:48 +0000

    moved table component

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,17 +1,11 @@
 import React from 'react';
 import {
-  Alert,
-  Image,
-  Platform,
-  ScrollView,
   StyleSheet,
-  Text,
-  TouchableOpacity,
   View,
 } from 'react-native';
 import { WebBrowser } from 'expo';
 
-import { MonoText } from '../components/StyledText';
+import { Table } from '../components/table/Table';
 
 export default class HomeScreen extends React.Component {
   static navigationOptions = {
@@ -22,59 +16,7 @@ export default class HomeScreen extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-          <View style={{ marginTop: 50, paddingLeft: 1, paddingRight: 1 }}>
-            <View style={styles.tableRowHeading}>
-              <View style={styles.tableCell}>
-                <Text style={{fontWeight: 'bold', color: '#444'}}>Skill</Text>
-              </View>
-              <View style={styles.tableCellMain}>
-                <Text style={{fontWeight: 'bold', color: '#444'}}>Success</Text>
-              </View>
-              <View style={styles.tableCellMain}>
-                <Text style={{fontWeight: 'bold', color: '#444'}}>Fail</Text>
-              </View>
-            </View>
-            <View style={styles.tableRow}>
-              <View style={styles.tableCellLarge}>
-                <Text>Pass</Text>
-              </View>
-              <TouchableOpacity 
-                style={styles.tableCellLargeMain}
-                onPress={() => Alert.alert('press')}
-              >
-                <View >
-                  <Text></Text>
-                </View>
-              </TouchableOpacity>
-              <TouchableOpacity style={styles.tableCellLargeMain}>
-                <View>
-                  <Text></Text>
-                </View>
-              </TouchableOpacity>
-            </View>
-            <View style={styles.tableRow}>
-              <View style={styles.tableCellLarge}>
-                <Text>Shoot</Text>
-              </View>
-              <View style={styles.tableCellLargeMain}>
-                <Text></Text>
-              </View>
-              <View style={styles.tableCellLargeMain}>
-                <Text></Text>
-              </View>
-            </View>
-            <View style={styles.tableRowBottom}>
-              <View style={styles.tableCellLarge}>
-                <Text>Tackle</Text>
-              </View>
-              <View style={styles.tableCellLargeMain}>
-                <Text></Text>
-              </View>
-              <View style={styles.tableCellLargeMain}>
-                <Text></Text>
-              </View>
-            </View>
-          </View>
+        <Table />
       </View>
     );
   }
@@ -84,56 +26,5 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-  },
-  tableRow: {
-    flexDirection: 'row', 
-    justifyContent: 'space-between', 
-    borderWidth: 1 ,
-    borderBottomWidth: 0,
-    borderRightWidth: 0,
-    borderColor: '#d3d3d3'
-  },
-  tableRowHeading: {
-    flexDirection: 'row', 
-    justifyContent: 'space-between', 
-    borderWidth: 1 ,
-    borderBottomWidth: 0,
-    borderRightWidth: 0,
-    borderColor: '#d3d3d3',
-  },
-  tableRowBottom: {
-    flexDirection: 'row', 
-    justifyContent: 'space-between', 
-    borderWidth: 1 ,
-    borderRightWidth: 0,
-    borderColor: '#d3d3d3'
-  },
-  tableCell: {
-    borderRightWidth: 1, 
-    padding: 10, 
-    flex: 1,
-    borderColor: '#d3d3d3'
-  },
-  tableCellMain: {
-    borderRightWidth: 1, 
-    padding: 10, 
-    flex: 2,
-    borderColor: '#d3d3d3'
-  },
-  tableCellLarge: {
-    borderRightWidth: 1, 
-    padding: 10,
-    paddingTop: 30,
-    paddingBottom: 30,
-    flex: 1,
-    borderColor: '#d3d3d3'
-  },
-  tableCellLargeMain: {
-    borderRightWidth: 1, 
-    padding: 10,
-    paddingTop: 30,
-    paddingBottom: 30,
-    flex: 2,
-    borderColor: '#d3d3d3'
   }
 });
