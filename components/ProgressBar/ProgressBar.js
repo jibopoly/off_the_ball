5e4f14a14ff21210a2618775c4f169f2f23d8e1a
Michael Close <michael.close@iesve.com>
2019-03-29 11:18:34 +0000

    progress bar style updates

--- components/ProgressBar/ProgressBar.js
+++ components/ProgressBar/ProgressBar.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { View, Text } from 'react-native';
+import { Ionicons } from '@expo/vector-icons';
 
 
 const styles = {
@@ -8,12 +9,14 @@ const styles = {
     width: 60,
     borderRadius: 30,
     borderWidth: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
   },
   line: {
     width: 50,
     height: 1,
     borderWidth: 1,
-    marginTop: 46,
+    marginTop: 48,
   },
 };
 
@@ -29,25 +32,43 @@ class ProgressBar extends React.Component {
     return (
       <View style={{ flexDirection: 'row', paddingTop: 20, justifyContent: 'center' }}>
 
-        <View style={{ flexDirection: 'column', borderWidth: 1, alignItems: 'center' }}>
+        <View style={{ flexDirection: 'column', alignItems: 'center' }}>
           <Text>Step 1</Text>
-          <View style={styles.circle} />
-          <Text>Step 1</Text>
+          <View style={styles.circle}>
+            <Ionicons
+              name="md-shirt"
+              size={24}
+              color="black"
+            />
+          </View>
+          <Text>Details</Text>
         </View>
 
         <View style={styles.line} />
-        <View style={{ flexDirection: 'column', borderWidth: 1, alignItems: 'center' }}>
-          <Text>Step 1</Text>
-          <View style={styles.circle} />
-          <Text>Step 1</Text>
+        <View style={{ flexDirection: 'column', alignItems: 'center' }}>
+          <Text>Step 2</Text>
+          <View style={styles.circle}>
+            <Ionicons
+              name="md-football"
+              size={24}
+              color="black"
+            />
+          </View>
+          <Text>Session</Text>
         </View>
 
         <View style={styles.line} />
 
-        <View style={{ flexDirection: 'column', borderWidth: 1, alignItems: 'center' }}>
-          <Text>Step 1</Text>
-          <View style={styles.circle} />
-          <Text>Step 1</Text>
+        <View style={{ flexDirection: 'column', alignItems: 'center' }}>
+          <Text>Step 3</Text>
+          <View style={styles.circle}>
+            <Ionicons
+              name="md-clipboard"
+              size={24}
+              color="black"
+            />
+          </View>
+          <Text>Evaluation</Text>
         </View>
 
       </View>
